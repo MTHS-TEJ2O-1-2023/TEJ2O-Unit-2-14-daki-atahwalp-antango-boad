2053c0becb9f72c9cf57c1fb2af60fa41c6e5c3c
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-07 13:03:55 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Daki A.B
  * Created on: Nov 2023
- * This program ...
+ * This program lights microbit lights diagonally 
 */
 
+// variables
+let loopCounter: number = 0
+let sprite: game.LedSprite = null
 
+// on button A, LED sprite lights up diagonally from top left corner to bottom right corner
+input.onButtonPressed(Button.A, function () {
+  // setting values
+  basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+
+  while (loopCounter <= 5) { 
+      sprite.set(LedSpriteProperty.X, loopCounter)
+      sprite.set(LedSpriteProperty.Y, loopCounter)
+      basic.pause(500)
+      loopCounter ++
+}
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
+})
+
+// on button B, LED sprite lights up diagonally from bottom right corner to top left corner
+input.onButtonPressed(Button.B, function () {
+  // setting values
+  basic.clearScreen()
+  loopCounter = 5
+  sprite = game.createSprite(5, 5)
+
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        basic.pause(500)
+        loopCounter--
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
